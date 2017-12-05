d4d14f02adf88057e0b8b7e9ca8aab67ccb23bee
Sven Langendries <sven.langendries@soundtalks.com>
2017-12-05 15:04:47 +0100

    Finish day 05

--- day_05/day_05.py
+++ day_05/day_05.py
@@ -1,5 +1,7 @@
 import os
 
+from copy import copy
+
 
 def jump_recursive(maze, current_idx=0, jump_counter=0):
     """This does not work for large inputs, will have to do an iterative function instead"""
@@ -17,20 +19,25 @@ def jump_recursive(maze, current_idx=0, jump_counter=0):
     return jump_counter, current_idx
 
 
-def jump(maze):
+def jump(maze, special_jump=False):
+    # We do a copy here because we want to re-use the original maze for printing the second half.
+    maze = copy(maze)
     current_idx = 0
     jump_counter = 0
 
     while True:
         offset = maze[current_idx]
-        position = current_idx
-        new_idx = position + offset
+        new_idx = current_idx + offset
 
-        maze[current_idx] = maze[current_idx] + 1
+        if special_jump and offset >= 3:
+            maze[current_idx] = maze[current_idx] - 1
+        else:
+            maze[current_idx] = maze[current_idx] + 1
+
         current_idx = new_idx
         jump_counter += 1
 
-        if current_idx >= len(maze):
+        if current_idx < 0 or current_idx >= len(maze):
             break
 
     return jump_counter
@@ -43,10 +50,15 @@ def main():
         maze = map(int, maze)
 
     print "Day 05, Half 01: {}".format(jump(maze))
+    print "Day 05, Half 02: {}".format(jump(maze, special_jump=True))
 
 
 def test_maze():
     assert jump([0, 3, 0, 1, -3]) == 5
+
+
+def test_maze_with_decrease_by_1_if_offset_3_or_more():
+    assert jump([0, 3, 0, 1, -3], special_jump=True) == 10
 
 
 if __name__ == '__main__':
--- run_all.py
+++ run_all.py
@@ -2,9 +2,11 @@ from day_01 import day_1
 from day_02 import day_2
 from day_03 import day_3
 from day_04 import day_4
+from day_05 import day_05
 
 if __name__ == '__main__':
     day_1.main()
     day_2.main()
     day_3.main()
     day_4.main()
+    day_05.main()
