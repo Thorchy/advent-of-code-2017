0f589d9e71a735801a554ac219e0eb22d2bf41ba
Sven Langendries <sven.langendries@soundtalks.com>
2017-12-06 11:29:09 +0100

    Add leading zeros to module name numbers

--- day_03/day_03.py
+++ day_03/day_03.py
@@ -1,3 +1,4 @@
+from itertools import count
 from math import sqrt
 
 PUZZLE_INPUT = 277678
@@ -23,7 +24,25 @@ def find_distance(value, base_value=1):
     return abs(x2 - x1) + abs(y2 - y1)
 
 
+def sum_spiral():
+    a, i, j = {(0, 0): 1}, 0, 0
+    for s in count(1, 2):
+        for (ds, di, dj) in [(0, 1, 0), (0, 0, -1), (1, -1, 0), (1, 0, 1)]:
+            for _ in range(s + ds):
+                i += di
+                j += dj
+                a[i, j] = sum(a.get((k, l), 0) for k in range(i - 1, i + 2)
+                              for l in range(j - 1, j + 2))
+                yield a[i, j]
+
+
+def part2(n):
+    for x in sum_spiral():
+        if x > n: return x
+
+
 def main():
+    print part2(PUZZLE_INPUT)
     print "Day 3, Half 1: {}".format(find_distance(PUZZLE_INPUT))
     print "Day 3, Half 2: {}".format('Not Yet Implemented')
 
@@ -42,6 +61,7 @@ def test_spiral_case_3():
 
 def test_spiral_case_4():
     assert find_distance(1024) == 31
+
 
 if __name__ == '__main__':
     main()
--- run_all.py
+++ run_all.py
@@ -1,12 +1,14 @@
-from day_01 import day_1
-from day_02 import day_2
-from day_03 import day_3
-from day_04 import day_4
+from day_01 import day_01
+from day_02 import day_02
+from day_03 import day_03
+from day_04 import day_04
 from day_05 import day_05
+from day_06 import day_06
 
 if __name__ == '__main__':
-    day_1.main()
-    day_2.main()
-    day_3.main()
-    day_4.main()
+    day_01.main()
+    day_02.main()
+    day_03.main()
+    day_04.main()
     day_05.main()
+    day_06.main()
